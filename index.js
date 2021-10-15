53af44fe6c0115c7f5ef6f4b0b6e682d0d141965
mbharti321 <mbharti321@gmail.com>
2021-10-15 13:22:38 +0530

    add eventListener to buttons

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // alert("hello");
+
+// document.querySelector("button").addEventListener("click", clickResponce);
+
+var buttons = document.querySelectorAll(".drum");
+// for( var i=0; i<buttons.length; i++){
+//     buttons[i].addEventListener("click", clickResponce);
+// }
+// function clickResponce(){
+//     alert("I was clicked!!");
+// }
+
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function () {
+        //         alert("I was clicked!!");
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
+
+    });
+}
